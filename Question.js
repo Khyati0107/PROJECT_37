53759557b487226578638bbb44c25fda5bce5732
Khyati0107 <78732262+Khyati0107@users.noreply.github.com>
2021-06-10 17:01:26 +0530

    Update Question.js

--- Question.js
+++ Question.js
@@ -2,8 +2,8 @@ class Question {
 
   constructor() {
     this.title = createElement('h1')
-    this.input1 = createInput("Name ");
-    this.input2 = createInput("Correct Option No..");
+    this.input1 = createInput("Enter Your Name Here....");
+    this.input2 = createInput("Enter Correct Option No..");
     this.button = createButton('Submit');
     this.question = createElement('h3');
     this.option1 = createElement('h4');
@@ -23,15 +23,15 @@ class Question {
     this.title.html("MyQuiz Game");
     this.title.position(350, 0);
 
-    this.question.html("Question:- Where is the international court of justice located? " );
+    this.question.html("Question:- What starts and ends with the letter ‘E’, but has only one letter? " );
     this.question.position(150, 80);
-    this.option1.html("1: Geneva " );
+    this.option1.html("1: Everyone " );
     this.option1.position(150, 100);
-    this.option2.html("2: Hague" );
+    this.option2.html("2: Envelope" );
     this.option2.position(150, 120);
-    this.option3.html("3: London " );
+    this.option3.html("3: Estimate " );
     this.option3.position(150, 140);
-    this.option4.html("4: New York" );
+    this.option4.html("4: Example" );
     this.option4.position(150, 160);
 
     this.input1.position(150, 230);
